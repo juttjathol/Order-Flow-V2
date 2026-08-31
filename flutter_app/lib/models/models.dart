7722065a9c48436a0cada9ec6cb3009234ef551b
juttjathol <aa1345471@gmail.com>
2026-08-31 20:13:23 +0800

    RESTORE full models.dart + localBt SessionPrefs

--- flutter_app/lib/models/models.dart
+++ flutter_app/lib/models/models.dart
@@ -1,8 +1,1 @@
-import 'package:uuid/uuid.dart';
-
-import '../core/constants.dart';
-
-const _uuid = Uuid();
-String newId() => _uuid.v4();
-
-// PLACEHOLDER - WILL REPLACE WITH FULL FILE
+file:///tmp/models_fixed.dart
