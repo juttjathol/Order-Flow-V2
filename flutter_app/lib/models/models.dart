29c0fc096e408cb7854959038253d4c1f936aa0a
juttjathol <aa1345471@gmail.com>
2026-08-31 20:12:55 +0800

    RESTORE models.dart from good commit (unbreak SEE_FILE)

--- flutter_app/lib/models/models.dart
+++ flutter_app/lib/models/models.dart
@@ -1,1 +1,8 @@
-@/tmp/models_good.dart
+import 'package:uuid/uuid.dart';
+
+import '../core/constants.dart';
+
+const _uuid = Uuid();
+String newId() => _uuid.v4();
+
+// PLACEHOLDER - WILL REPLACE WITH FULL FILE
